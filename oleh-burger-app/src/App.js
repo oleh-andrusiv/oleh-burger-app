282fc560cbf2ac59f3b22167bdd2dd3cbd7566f8
oleh-andrusiv <andrusiv.o.m@gmail.com>
2022-12-10 23:42:03 +0200

    prototype finished

--- oleh-burger-app/src/App.js
+++ oleh-burger-app/src/App.js
@@ -1,23 +1,72 @@
 import logo from './logo.svg';
+import slider from './uaf.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-header_text">
+          Help to UAF
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-header_nav">
+          <a
+            className="App-header_link"
+            href="https://savelife.in.ua/projects/military/mobilni-kompleksy-sposterezhennya/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Donate
+          </a>
+          <a
+            className="App-header_link"
+            href="https://savelife.in.ua/projects/military/mobilni-kompleksy-sposterezhennya/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Project
+          </a>
+          <a
+            className="App-header_link"
+            href="https://savelife.in.ua/projects/military/mobilni-kompleksy-sposterezhennya/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          About
+          </a>
+          <a
+            className="App-header_link"
+            href="https://savelife.in.ua/materials/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Materials
+          </a>
+        </div>
+      </div>
+      <div className="App-slider">
+        <img src={slider} className="App-slider_img" alt="Full width banner" />
+      </div>
+      <div className="App-materials">
+        <div className="App-materials_blog">
+          <p className="App-materials_header">Blog</p>
+          <div className="App-materials_blog_items-list">
+            <p className="App-materials_blog_item">Коли Україна остаточно відбере ініціативу у росіян</p>
+            <p className="App-materials_blog_item">Як змінювалась світова допомога Україні за 100 днів протистояння</p>
+            <p className="App-materials_blog_item">Що робити з трофеями, які здобувають українські захисники</p>
+            <p className="App-materials_blog_item">Потреби ветеранів у сприянні підприємницької діяльності</p>
+          </div>
+        </div>
+        <div className="App-materials_news">
+          <p className="App-materials_header">News</p>
+          <div className="App-materials_news_items-list">
+            <p className="App-materials_news_item">«Виклик для США - забезпечити контрольований занепад росії»</p>
+            <p className="App-materials_news_item">Запустили онлайн-курс для операторів дронів</p>
+            <p className="App-materials_news_item">Придбали війську тепловізійної оптики на 3,3 млн євро</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
